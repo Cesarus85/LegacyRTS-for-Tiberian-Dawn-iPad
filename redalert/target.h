TheAssemblyArmada/Vanilla-Conquer

Fix a narrowing conversion

redalert/target.h
@@ -165,7 +165,7 @@ public:
     void Invalidate(void)
     {
         Target.Sub.Exponent = RTTI_NONE;
-        Target.Sub.Mantissa = -1;
+        Target.Sub.Mantissa = (1UL << TARGET_MANTISSA) - 1;
     }
     bool Is_Valid(void) const
     {

tiberiandawn/target.h
@@ -183,7 +183,7 @@ public:
     void Invalidate(void)
     {
         Target.Sub.Exponent = RTTI_NONE;
-        Target.Sub.Mantissa = -1;
+        Target.Sub.Mantissa = (1UL << TARGET_MANTISSA) - 1;
     }
     bool Is_Valid(void) const
     {

Cesarus85/LegacyRTS-for-Tiberian-Dawn-iPad

redalert/target.h
@@ -165,7 +165,7 @@ public:
     void Invalidate(void)
     {
         Target.Sub.Exponent = RTTI_NONE;
-        Target.Sub.Mantissa = -1;
+        Target.Sub.Mantissa = (1U << TARGET_MANTISSA) - 1;
     }
     bool Is_Valid(void) const
     {

tiberiandawn/target.h
@@ -183,7 +183,7 @@ public:
     void Invalidate(void)
     {
         Target.Sub.Exponent = RTTI_NONE;
-        Target.Sub.Mantissa = -1;
+        Target.Sub.Mantissa = (1U << TARGET_MANTISSA) - 1;
     }
     bool Is_Valid(void) const
     {